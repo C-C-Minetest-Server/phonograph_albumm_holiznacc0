ce09fb19ea9a47d3444b00e31a417782118a8467
1F616EMO <root@1f616emo.xyz>
2026-01-20 17:30:27 +0800

    Add stereo channels

--- src/001-background-music.lua
+++ src/001-background-music.lua
@@ -24,92 +24,35 @@ local album = phonograph.register_album("phonograph_album_holiznacc0:001-backgro
     license = phonograph.licenses.CC0,
 })
 
-album:register_song("01-creppy-piano-1", {
-    title = S("Creppy Piano 1"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("01-creppy-piano-1"),
-    spec = {},
-})
+for _, data in ipairs {
+    { "01-creppy-piano-1",                  S("Creppy Piano 1"),                    nil },
+    { "02-creppy-piano-2",                  S("Creppy Piano 2"),                    nil },
+    { "03-creppy-piano-3",                  S("Creppy Piano 3"),                    nil },
+    { "04-creppy-piano-4",                  S("Creppy Piano 4"),                    nil },
+    { "05-dangerous-voyage",                S("Dangerous Voyage"),                  nil },
+    { "06-dangerous-voyage-music-box",      S("Dangerous Voyage (Music Box)"),      nil },
+    { "07-a-small-town-on-pluto-music-box", S("A Small Town on Pluto (Music Box)"), nil },
+    { "08-a-small-town-on-pluto-composed",  S("A Small Town on Pluto (Composed)"),  nil },
+    { "09-cabin-fever",                     S("Cabin Fever"),                       nil },
+    { "10-spring-on-the-horizon",           S("Spring On The Horizon"),             nil },
+} do
+    local id, title, short_description = unpack(data)
+    album:register_song(id, {
+        title = title,
+        short_description = short_description,
+        long_description = nil,
+        artist = nil,
+        spec = {
+            filepath = songpath(id),
+        },
+        multichannel_specs = {
+            {
+                filepath = songpath(id .. "_ch0"),
+            },
+            {
+                filepath = songpath(id .. "_ch1"),
+            },
+        },
 
-album:register_song("02-creppy-piano-2", {
-    title = S("Creppy Piano 2"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("02-creppy-piano-2"),
-    spec = {},
-})
-
-album:register_song("03-creppy-piano-3", {
-    title = S("Creppy Piano 3"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("03-creppy-piano-3"),
-    spec = {},
-})
-
-album:register_song("04-creppy-piano-4", {
-    title = S("Creppy Piano 4"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("04-creppy-piano-4"),
-    spec = {},
-})
-
-album:register_song("05-dangerous-voyage", {
-    title = S("Dangerous Voyage"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("05-dangerous-voyage"),
-    spec = {},
-})
-
-album:register_song("06-dangerous-voyage-music-box", {
-    title = S("Dangerous Voyage (Music Box)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("06-dangerous-voyage-music-box"),
-    spec = {},
-})
-
-album:register_song("07-a-small-town-on-pluto-music-box", {
-    title = S("A Small Town on Pluto (Music Box)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("07-a-small-town-on-pluto-music-box"),
-    spec = {},
-})
-
-album:register_song("08-a-small-town-on-pluto-composed", {
-    title = S("A Small Town on Pluto (Composed)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("08-a-small-town-on-pluto-composed"),
-    spec = {},
-})
-
-album:register_song("09-cabin-fever", {
-    title = S("Cabin Fever"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("09-cabin-fever"),
-    spec = {},
-})
-
-album:register_song("10-spring-on-the-horizon", {
-    title = S("Spring On The Horizon"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("10-spring-on-the-horizon"),
-    spec = {},
-})
+    })
+end
--- src/002-winter-lofi.lua
+++ src/002-winter-lofi.lua
@@ -19,52 +19,36 @@ local album = phonograph.register_album("phonograph_album_holiznacc0:002-winter-
     title = S("Winter Lofi"),
     short_description = nil,
     long_description = nil,
-    -- cover = "phonograph_album_holiznacc0_cover_001.jpg",
+    -- cover = "phonograph_album_holiznacc0_cover_002.jpg",
     artist = S("HoliznaCC0"),
     license = phonograph.licenses.CC0,
 })
 
-album:register_song("01-2-hour-delay", {
-    title = S("2 Hour Delay"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("01-2-hour-delay"),
-    spec = {},
-})
 
-album:register_song("02-fire-place", {
-    title = S("Fire Place"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("02-fire-place"),
-    spec = {},
-})
+for _, data in ipairs {
+    { "01-2-hour-delay", S("2 Hour Delay"), nil },
+    { "02-fire-place",   S("Fire Place"),   nil },
+    { "03-snow-drift",   S("Snow Drift"),   nil },
+    { "04-first-snow",   S("First Snow"),   nil },
+    { "05-winter-blues", S("Winter Blues"), nil },
+} do
+    local id, title, short_description = unpack(data)
+    album:register_song(id, {
+        title = title,
+        short_description = short_description,
+        long_description = nil,
+        artist = nil,
+        spec = {
+            filepath = songpath(id),
+        },
+        multichannel_specs = {
+            {
+                filepath = songpath(id .. "_ch0"),
+            },
+            {
+                filepath = songpath(id .. "_ch1"),
+            },
+        },
 
-album:register_song("03-snow-drift", {
-    title = S("Snow Drift"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("03-snow-drift"),
-    spec = {},
-})
-
-album:register_song("04-first-snow", {
-    title = S("First Snow"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("04-first-snow"),
-    spec = {},
-})
-
-album:register_song("05-winter-blues", {
-    title = S("Winter Blues"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("05-winter-blues"),
-    spec = {},
-})
+    })
+end
